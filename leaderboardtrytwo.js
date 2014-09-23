8f8e20218425dd650e89ae0dc67249d82772f979
Anna Tong <tonga@ripple.com>
2014-09-22 19:25:17 -0700

    update s

--- leaderboardtrytwo.js
+++ leaderboardtrytwo.js
@@ -1,5 +1,5 @@
 //make an one-dimensional array 
- 
+
 var accounts= []
 var addresses = ["rKRWe49WhaVcCgp6L5ZDsWMagZzDvw3P9S", "rhgQGpna3XAmstXsq6gSbq3RBBp1UyCuwh"]
 
@@ -18,17 +18,17 @@ var request = require("request");
 
 var gettingbalances=function(i){
 	request("http://localhost:5990/v1/accounts/"+accounts[i].address+"/balances", 
-	function(error,response,body){
-	console.log (body);
-	body=JSON.parse(body)
-	accounts[i].balances=body.balances
-	if (i<accounts.length-1)
-		gettingbalances(i+1)
-	else {
-		console.log("done")
+		function(error,response,body){
+			console.log (body);
+			body=JSON.parse(body)
+			accounts[i].balances=body.balances
+			if (i<accounts.length-1)
+				gettingbalances(i+1)
+			else {
+				console.log("done")
 	//do the conversion
-		convert(0)
-	}
+	convert(0)
+}
 });
 }
 gettingbalances(0);
@@ -38,39 +38,53 @@ gettingbalances(0);
 //for each balance, look up the value of each balance in XRP, if not XRP
 //add the balances together
 
+var nonXRPvalue=[]
 
 var getexchange=function(base, baseissuer){
-request.post({
-	url:"http://api.ripplecharts.com/api/exchange_rates",
-	json: {
-    	pairs : [
-        	{
-        		base : {currency:base, "issuer":baseissuer},
-            	counter : {currency:"XRP"}
+	request.post({
+		url:"http://api.ripplecharts.com/api/exchange_rates",
+		json: {
+			pairs : [
+			{
+				base : {currency:base, "issuer":baseissuer},
+				counter : {currency:"XRP"}
 
-        	}
-    	]
-	}},
-	function(error,response,body){
-	console.log (body);
-	console.log (body.rate);
-	var obj = body[0]
-	console.log (obj.rate)
-	}
-)
+			}
+			]
+		}},
+		function(error,response,body){
+			//console.log (obj.rate, "sexybody");
+
+		//	console.log (body.rate);
+			var obj = body[0]
+			nonXRPvalue.push(obj.value)
+			console.log (body, "sexybody")
+			console.log (obj.rate, "sexyrate")
+			return Number(obj.rate)
+		}
+		)
 }
+
 
 
 var convert=function(){
-accounts.forEach(function(account){
-			account.balances.forEach( function(object){ 
+	accounts.forEach(function(account){
+
+		account.balances.forEach( function(object){ 
+
 			if (object.currency != "XRP") {
-				getexchange(object.currency,object.counterparty)
+
+				console.log(getexchange(object.currency,object.counterparty), "convertedbalance")
+
 			}
 		})
-		})
+	})
 }
 
+setTimeout(function(){
+	console.log('accccccounts', accounts[1].balances);
+	console.log('sexybalance',nonXRPvalue)
+}, 2000);
 
 
 
